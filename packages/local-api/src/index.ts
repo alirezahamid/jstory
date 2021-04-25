9b57c784ac648ff67762933c3c5a9475ef51ea83
Alireza Hamid <alirezahamid20@gmail.com>
2021-04-25 21:05:57 +0430

    create cli with lerna

---
+++ packages/local-api/src/index.ts
@@ -0,0 +1,5 @@
+export const serve = (port:number, filename: string, dir: string) => {
+  console.log('Serving on port: ', port)
+  console.log('Fetching cells from: ', filename)
+  console.log('Dir ', dir)
+}
